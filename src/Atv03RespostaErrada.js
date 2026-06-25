f36dff98850c46b253773a4bd0b986136cde4e9f
JonhPerter07 <joaopedro.rodrigues@nave.org.br>
2026-06-25 08:59:47 -0300

    Update Atv03RespostaErrada.js
    desculpa

--- src/Atv03RespostaErrada.js
+++ src/Atv03RespostaErrada.js
@@ -19,14 +19,3 @@
   * exportar o objeto do servidor para os testes automatizados.
   */
 
-if (personagens.length > 5) {
-  res.send(
-    "Quantidade de personagens escolhida superior ao necessário (5)"
-  );
-}
-
-if (personagens.length < 5) {
-  res.send(
-    "Quantidade de personagens escolhida inferior ao necessário (5)"
-  );
-}
