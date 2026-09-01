ff415a4f0e4188601f4b5e15b24ffa9de406dc52
rayRayanjoS <rayane.anjos@nave.org.br>
2026-09-01 07:55:28 -0300

    Update Atv03RespostaErrada.js

--- src/Atv03RespostaErrada.js
+++ src/Atv03RespostaErrada.js
@@ -20,14 +20,71 @@
   */
 
 
-if (personagens.length > 5) {
-  res.send(
-    "Quantidade de personagens escolhida superior ao necessário (5)"
-  );
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'));
+});
+
+app.get('/cadastro', (req, res) => {
+  const { nome, email, controle } = req.query;
+
+  let { personagem } = req.query;
+
+  // garante que personagem seja sempre um array
+if (!Array.isArray(personagem)) {
+  if (personagem) {
+    personagem = [personagem];
+  } else {
+    personagem = [];
+  }
 }
 
-if (personagens.length < 5) {
-  res.send(
-    "Quantidade de personagens escolhida inferior ao necessário (5)"
-  );
-}
+
+   if (personagem.length > 5) {
+    return res.send(
+      "Quantidade de personagens escolhida superior ao necessário (5)"
+    );
+  }
+
+  if (personagem.length < 5) {
+    return res.send(
+      "Quantidade de personagens escolhida inferior ao necessário (5)"
+    );
+  }
+
+  res.send(`
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="color-scheme" content="light dark"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+  <title>Prazer em lhe conhecer</title>
+</head>
+<body>
+  <h1>Cadastro feito com sucesso!</h1>
+  <p><strong>Participante:</strong> ${nome}</p>
+  <p><strong>Email:</strong> ${email}</p>
+  <p><strong>Tipo de controle:</strong> ${controle}</p>
+  <p><strong>Personagens escolhidos:</strong></p>
+  <p>${personagem[0]}</p>
+  <p>${personagem[1]}</p>
+  <p>${personagem[2]}</p>
+  <p>${personagem[3]}</p>
+  <p>${personagem[4]}</p>
+</body>
+</html>
+  `);
+});
+
+
+
+export default app
